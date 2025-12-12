cd36cf15fbcc4fb27ad10d0a1aeb71b346803ccb
albertocadena3890-ux <albertocadena3890@gmail.com>
2025-12-12 15:38:02 -0500

    Guardando mis cambios

--- bischoletos-practica/sprite_2d.gd
+++ bischoletos-practica/sprite_2d.gd
@@ -1,12 +1,17 @@
-extends Node
+extends Sprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
+var speed = 200
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-@export  
+	
+	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	position += direction * speed * delta
+
+
+  
