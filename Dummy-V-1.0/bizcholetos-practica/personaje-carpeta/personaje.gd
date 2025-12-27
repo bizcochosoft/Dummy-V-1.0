8faa5d8392636564d5487e6931faabdd632eaa0f
albertocadena3890-ux <albertocadena3890@gmail.com>
2025-12-27 18:43:56 -0500

    mensaje

--- Dummy-V-1.0/bizcholetos-practica/personaje-carpeta/personaje.gd
+++ Dummy-V-1.0/bizcholetos-practica/personaje-carpeta/personaje.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 class_name personaje
 
-@export var gravity = 1200
-@export var jump_velocity = 900
-@export var speed = 200
-@export var demasiado_abajo = 950
+@export var gravity = 250
+@export var jump_velocity = 230
+@export var speed = 74
+@export var demasiado_abajo = 185
+
 
 signal caer
 
@@ -28,7 +29,51 @@ func _physics_process(delta):
 		queue_free()
 		caer.emit()
 	
+	
 	move_and_slide()
+	
+	
+	for i in get_slide_collision_count():
+		var colision = get_slide_collision(i)
+		var objetoChocado = colision.get_collider()
+		
+		
+		if objetoChocado == $"../TileMapLayer":
+			var punto_golpe = colision.get_position()
+			var normal = colision.get_normal()
+			
+			
+			
+			if normal.y < 0:
+				
+				
+				var pos_para_revisar = punto_golpe - (normal * 4)
+				
+				var pos_local = objetoChocado.to_local(pos_para_revisar)
+				var coord_mapa = objetoChocado.local_to_map(pos_local)
+				
+				var datos = objetoChocado.get_cell_tile_data(coord_mapa)
+				
+				if datos and datos.get_custom_data("esRompible"):
+					objetoChocado.erase_cell(coord_mapa)
+
+ 
+					
+					#
+
+
+	
+	
+	
+	var tamano_pantalla = get_viewport_rect().size
+	print( get_viewport_rect().size)
+	
+	if global_position.x > tamano_pantalla.x:
+		global_position.x=0
+	if global_position.x < 0 :
+		global_position.x= tamano_pantalla.x
+		
+
 
 func _on_area_2d_body_entered(body):
 	print("El ", body.name, " ha entrado en el área")
